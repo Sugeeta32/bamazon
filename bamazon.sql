5b38417edfa7304b7f148e9c9da69adc8aa0b46a
Sugeeta32 <sugeeta.gupta@gmail.com>
2019-11-13 13:06:49 -0500

    Images

---
+++ bamazon.sql
@@ -0,0 +1,12 @@
+create database bamazon_db;
+use bamazon_db;
+
+create table products(
+    item_id int not null auto_increment,
+    product_name varchar(50) not null,
+    department_name varchar(50) not null,
+    price decimal(10,2) not null,
+    stock_quantity int(10) not null,
+    primary key(item_id)
+);
+
